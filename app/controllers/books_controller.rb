c3d79795e792c003d9f905ef4df514b5c5c9bfb8
Nigel Baillie <nigelbaillie@hey.com>
2021-03-20 19:23:26 +0900

    Really rudimentary book viewer

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,9 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find_by!(slug: params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   mount Reader::API => '/'
+
+  resources :books, only: [:index, :show]
 end
