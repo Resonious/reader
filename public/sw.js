53f9cce21765df6af546fc2635d63b5fd6a92c60
Nigel Baillie <metreckk@gmail.com>
2021-09-20 12:21:49 +0900

    More cache tweaks!!

--- public/sw.js
+++ public/sw.js
@@ -4,15 +4,29 @@ self.addEventListener('install', () => {
   console.log('[Service Worker] install!')
 })
 
+function returnCacheImmediately(url) {
+  return url.endsWith('.js') ||
+    url.endsWith('.css') ||
+    url.includes('/lookup/');
+}
+
+function dontSaveToCache(url) {
+  return url.endsWith('/whoareyou');
+}
+
 // We do a real fetch everytime, returning a cache in case of network errors.
 self.addEventListener('fetch', (event) => {
   event.respondWith((async () => {
+    // Only muck around with cache for GET requests
+    if (event.request.method !== 'GET')
+      return fetch(event.request);
+
     console.log(event.request);
     const url = event.request.url;
 
     try {
       // Try cache first, only if it is an asset or dict lookup
-      if (url.endsWith('.js') || url.endsWith('.css') || url.includes('/lookup/')) {
+      if (returnCacheImmediately(url)) {
         const cachedResponse = await caches.match(event.request);
         if (cachedResponse) return cachedResponse;
       }
@@ -22,7 +36,7 @@ self.addEventListener('fetch', (event) => {
       if (!realResponse.ok) return realResponse;
 
       // Stick it in the cache if it was OK
-      if (realResponse.ok) {
+      if (!dontSaveToCache(url) && realResponse.ok) {
         const cache = await caches.open(cacheName);
         console.log(`[Service Worker] Caching new resource: ${url}`);
         cache.put(event.request, realResponse.clone());
